8ee5b59880b446ac80e7715dad98dc1b09521b62
David Caro <dcaro@wikimedia.org>
2022-07-12 12:09:56 +0200

    Extract function so it can be pickled

--- celery_app.py
+++ celery_app.py
@@ -1,18 +1,16 @@
+import subprocess
 from cv_model_serve import create_app, ext_celery
 
 
 app = create_app()
 celery = ext_celery.celery
 
+def run_worker():
+    subprocess.call(
+        ["celery", "-A", "celery_app.celery", "worker", "--loglevel=info"]
+    )
 
 @app.cli.command("celery_worker")
 def celery_worker():
     from watchgod import run_process
-    import subprocess
-
-    def run_worker():
-        subprocess.call(
-            ["celery", "-A", "celery_app.celery", "worker", "--loglevel=info"]
-        )
-
     run_process("./cv_model_serve", run_worker)
